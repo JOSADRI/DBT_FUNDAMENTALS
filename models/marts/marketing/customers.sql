6d1ac8f4efd1a4e3a5d4957543ec278628285aef
ADRIAN TORRES RODA <adriantrabajo2000@gmail.com>
2025-07-04 17:50:18 +0000

    EX_PRAC_LIFETIME_VALUE

--- models/marts/marketing/customers.sql
+++ models/marts/marketing/customers.sql
@@ -4,14 +4,18 @@ with customers as (
 orders as (
     SELECT * FROM {{ ref('stg_orders') }}
 ),
+payments as (
+    SELECT * FROM {{ ref('stg_payments') }}
+),
 customer_order as (
     SELECT
-        CUSTOMER_ID,
-        MIN(ORDER_DATE) AS FIRST_ORDER_DATE,
-        MAX(ORDER_DATE) AS MOST_RECENT_ORDER_DATE,
-        COUNT(ORDER_ID) AS NUMBER_OF_ORDERS
+        A.CUSTOMER_ID,
+        MIN(A.ORDER_DATE) AS FIRST_ORDER_DATE,
+        MAX(A.ORDER_DATE) AS MOST_RECENT_ORDER_DATE,
+        COUNT(A.ORDER_ID) AS NUMBER_OF_ORDERS,
+        SUM(B.AMOUNT) AS LIFETIME_VALUE
     FROM
-        orders
+        orders A INNER JOIN payments B USING(CUSTOMER_ID)
     GROUP BY 1
 ),
 final as (
@@ -21,7 +25,9 @@ final as (
         customers.LAST_NAME,
         customer_order.FIRST_ORDER_DATE,
         customer_order.MOST_RECENT_ORDER_DATE,
-        coalesce(customer_order.NUMBER_OF_ORDERS,0) as NUMBER_OF_ORDERS
+        coalesce(customer_order.NUMBER_OF_ORDERS,0) as NUMBER_OF_ORDERS,
+        LIFETIME_VALUE
+
     FROM
         customers LEFT JOIN customer_order USING(CUSTOMER_ID)
 )
